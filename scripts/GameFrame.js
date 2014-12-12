e56146141320b19bcffa30c5f2ca944ee1d58bd0
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-11 21:27:05 -0500

    Established the framework for toggling gamestates.

--- scripts/GameFrame.js
+++ scripts/GameFrame.js
@@ -1,11 +1,37 @@
 var GameFrame = React.createClass({
+    getInitialState: function() {
+        return {
+            currentGamestate: TitlescreenGamestate
+        }
+    },
     render: function() {
+        var Gamestate = this.state.currentGamestate
         return (
             <div id="game-frame">
+                <Gamestate/>
+            </div>
+        )
+    }
+})
+
+var TitlescreenGamestate = React.createClass({
+    render: function() {
+        return (
+            <div>
                 Hello World!
             </div>
-        );
+        )
     }
-});
+})
 
-module.exports = GameFrame;
+var PlayingGamestate = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Goodbye World!
+            </div>
+        )
+    }
+})
+
+module.exports = GameFrame
