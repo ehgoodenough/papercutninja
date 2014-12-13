d6c6a5615b0132a285a96f0eccfed9032227a536
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-13 12:50:13 -0500

    Scrolled the level.

--- scripts/CameraStore.js
+++ scripts/CameraStore.js
@@ -1,5 +1,7 @@
 var NinjaStore = require("./NinjaStore")
 
+var LEVEL_WIDTH = 60;
+
 var CameraStore = Reflux.createStore({
     state: {
         position: {
@@ -12,7 +14,14 @@ var CameraStore = Reflux.createStore({
     },
     init: function() {
         this.listenTo(NinjaStore, function(state) {
-            this.state.position.x = state.position.x * -1
+            this.state.position.x = (state.position.x - (2 * 3)) * -1
+            console.log(this.state.position.x)
+            if(this.state.position.x > 0) {
+                this.state.position.x = 0
+            }
+            if(this.state.position.x < LEVEL_WIDTH * -1) {
+                this.state.position.x = LEVEL_WIDTH * -1
+            }
             this.trigger(this.state)
         }.bind(this))
     }
